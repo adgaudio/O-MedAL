aba04eac0ac48a16da39089645f990fe0eca0929
Alex Gaudio <adgaudio@gmail.com>
2019-02-18 19:31:34 -0500

    bridges tweak again

--- bridges/fetch_log_from_bridges.sh
+++ bridges/fetch_log_from_bridges.sh
@@ -3,9 +3,6 @@
 set -e
 set -u
 
-bridges_user=${1:-no}
-parse_log=${2:-no}
+bridges_user=${1}
 
-if [ "$bridges_user" != "no" ] ; then
-  rsync -ave ssh $bridges_user@data.bridges.psc.edu:/pylon5/ci4s8dp/$bridges_user/medal_improvements/data/log ./data/
-fi
+rsync -ave ssh $bridges_user@data.bridges.psc.edu:/pylon5/ci4s8dp/$bridges_user/medal_improvements/data/log ./data/
