3952ea0c2634f4a807ef4f2f3a5c9892878fc9a3
Alex Gaudio <adgaudio@gmail.com>
2019-02-19 19:30:12 -0500

    fetch script: only overwrite relevant logs

--- bridges/fetch_log_from_bridges.sh
+++ bridges/fetch_log_from_bridges.sh
@@ -4,13 +4,16 @@ set -e
 set -u
 
 bridges_user=${1}
-parse_all_logs=${2:-yes}
 
 # cd into parent directory of the script
 cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-rsync -ave ssh $bridges_user@data.bridges.psc.edu:/pylon5/ci4s8dp/$bridges_user/medal_improvements/data/log ./data/
+# copy data
+fps="$(rsync -ae ssh --out-format="%f\n" $bridges_user@data.bridges.psc.edu:/pylon5/ci4s8dp/$bridges_user/medal_improvements/data/log ./data/)"
+echo fetched:
+echo -e $fps
 
-if [ "$parse_all_logs" = "yes" ] ; then
-  ./bin/parse_all_logs.sh
-fi
+# generate plots of copied data
+. ./bin/bash_lib.sh
+export -f run_parselog_py
+echo -e $fps |  grep / | awk '{$1=$1};1' | parallel run_parselog_py overwrite data/{}
